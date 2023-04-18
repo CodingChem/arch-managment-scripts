2f944adfa88ba9a3d13e97e3b74f8c35ab57c5bb
vegard <vegsei@gmail.com>
2023-04-18 09:03:03 +0200

    a script to resett a arch install to bare bones

---
+++ reset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# become su
+su -
+# Mark all packages as dependancies
+pacman -D --asdeps $(pacman -Qqe)
+
+pacman -D --asexplicit base base-devel linux linux-firmware git vim 
+
+# for intel
+pacman -D --asexplicit intel-ucode
+
+# for nvidia
+pacman -D --asexplicit nvidia nvidia-utils nvidia-settings
+
+# for wireless
+pacman -D --asexplicit networkmanager
+
+pacman -Qttdq | pacman -Rns -
