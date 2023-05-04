c2ae4219dde68f92ca3108729f30ad8d34ed1819
Vegard <vegsei@gmail.com>
2023-05-04 12:36:01 +0200

    Update install-base.sh

--- install-base.sh
+++ install-base.sh
@@ -16,11 +16,7 @@ echo "127.0.1.1 nitro5.localdomain nitro5" >> /etc/hosts
 # setting root password to password
 echo root:password | chpasswd
 
-pacman -S grub efibootmgr networkmanager base-devel linux-headers bluez bluez-utils pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse bash-completion openssh reflector nvidia nvidia-utils nvidia-settings xorg-server xorg-xinit
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
-
-grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S efibootmgr networkmanager base-devel linux-headers bluez bluez-utils pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse bash-completion openssh reflector nvidia nvidia-utils nvidia-settings xorg-server xorg-xinit
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
